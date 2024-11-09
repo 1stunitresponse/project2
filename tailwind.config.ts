627645def87cca2bf6805ec842f0fd152f98bc65
JenisheSalem <jenishedufsalem@gmail.com>
2024-11-09 22:51:13 +0800

    install libraries - tailwind, flowbite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,37 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from "tailwindcss/defaultTheme";
+import flowbitePlugin from "flowbite/plugin";
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}',"./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
+	darkMode: "selector",
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+			},
+
+			colors: {
+
+			primary: {
+
+			"50": '#fff1f2',
+			"100": '#ffe4e6',
+       		"200": '#fecdd3',
+       		"300": '#fda4af',
+        	"400": '#fb7185',
+        	"500": '#f43f5e',
+        	"600": '#e11d48',
+        	"700": '#be123c',
+        	"800": '#9f1239',
+        	"900": '#881337',
+			"950": '#4c0519',
+
+			}
+			}
+		}
 	},
 
-	plugins: []
+	plugins: [flowbitePlugin]
 } satisfies Config;
